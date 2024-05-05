6f68a27d92cad68ec7023664cf607be8fc8e93e1
𝓜𝖊𝓓𝓸𝓞 <104529732+MeDoO152@users.noreply.github.com>
2024-05-06 00:37:06 +0300

    Update مين في الكول.py

--- ZeMusic/plugins/play/مين في الكول.py
+++ ZeMusic/plugins/play/مين في الكول.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from ZeMUSIC import app
+from import app
 from config import OWNER_ID
 import asyncio
 from pytgcalls import PyTgCalls, StreamType
