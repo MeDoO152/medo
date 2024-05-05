66a8b128fca4bef3a8876b449ca790e6fbf017ee
𝓜𝖊𝓓𝓸𝓞 <104529732+MeDoO152@users.noreply.github.com>
2024-05-05 21:37:45 +0300

    Add files via upload

---
+++ ZeMusic/plugins/play/تليغراف.py
@@ -0,0 +1,56 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from telegraph import upload_file
+import os
+from strings.filters import command
+from ZeMusic import app
+
+
+@app.on_message(filters.command(["تلغراف", "تلغراف ميديا", "ميديا", "تلجراف", "تليجراف"]) & filters.group)
+async def telegraph(client: Client, message: Message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.reply("🤕 ¦ الرد على ملف وسائط مدعوم\n• حط صوره و اكتب عليها 00")
+        return
+    if not (
+        (replied.photo and replied.photo.file_size <= 5242880)
+        or (replied.animation and replied.animation.file_size <= 5242880)
+        or (
+            replied.video
+            and replied.video.file_name.endswith(".mp4")
+            and replied.video.file_size <= 5242880
+        )
+        or (
+            replied.document
+            and replied.document.file_name.endswith(
+                (".jpg", ".jpeg", ".png", ".gif", ".mp4"),
+            )
+            and replied.document.file_size <= 5242880
+        )
+    ):
+        await message.reply("غير مدعوم!!")
+        return
+    download_location = await client.download_media(
+        message=message.reply_to_message,
+        file_name="root/downloads/",
+    )
+    try:
+        response = upload_file(download_location)
+    except Exception as document:
+        await message.reply(message, text=document)
+    else:
+        await message.reply(
+            f"<b>• الــرابـط:-</b>\n\n <code>https://telegra.ph{response[0]}</code>",
+            quote=True,
+            reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="🎯 ¦ افـتح الـرابـط", url=f"https://telegra.ph{response[0]}"),
+                    InlineKeyboardButton(text="♻️ ¦ مشـاركه الـرابـط", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
+                ],
+            ]
+        )
+    )
+    finally:
+        os.remove(download_location)
+
